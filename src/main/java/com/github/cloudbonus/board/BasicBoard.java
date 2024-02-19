eff136bf7597c1f07a8bc3e65c260336e5618842
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-02-19 13:39:38 +0300

    fix/PP-9 - Fix remaining ships on display

--- src/main/java/com/github/cloudbonus/board/BasicBoard.java
+++ src/main/java/com/github/cloudbonus/board/BasicBoard.java
@@ -54,7 +54,6 @@ public class BasicBoard extends Board{
     }
 
     public String getRemainingShips() {
-        List<Integer> remainingShipSizes = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6));
         Map<Integer, Integer> countMap = new HashMap<>();
         StringBuilder sb = new StringBuilder();
 
